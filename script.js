a2ea78c2328ecd6cb83e2ebfbfb7c851089ac1fa
Annabelle Perbet <annabelleperbet@yahoo.fr>
2026-05-07 15:34:17 +0200

    Mise en place de la logique pour la gestion de la soumission du formulaire

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Récupération des éléments HTML5
 const scrollIndicator = document.querySelector("#scroll__indicator");
+const email = document.querySelector(".email__input");
+const btn = document.querySelector(".submit__btn");
+const form = document.querySelector("form");
 
 // Déclaration de la fonction handleScrollIndicator qui va permettre de gérer la progression de la barre de scroll
 const handleScrollIndicator = () => {
@@ -19,3 +22,53 @@ const handleScrollIndicator = () => {
 
 // Ecoute de l'événement "scroll" sur la fenêtre et appel de la fonction handleScrollIndicator
 window.addEventListener("scroll", handleScrollIndicator);
+
+// Regex
+const regexEmail = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
+
+/**
+ * Déclaration de la fonction emailValidation pour la validation du champ nom
+ *  @param {String} email
+ */
+const emailValidation = (email) => {
+  // Ecoute de l'événement "input" sur l'input email
+  email.addEventListener("input", (e) => {
+    e.preventDefault();
+    if (regexEmail.test(email.value) === false) {
+      document.querySelector(".error__message").textContent =
+        "Check your email please";
+      email.style.borderColor = "#ff0000";
+      return false;
+    } else {
+      document.querySelector(".error__message").textContent = " ";
+      email.style.borderColor = "#00ff0d";
+      return true;
+    }
+  });
+};
+// Appel de la fonction emailValidation
+emailValidation(email);
+
+// Déclaration de la fonction submit gérant la soumission du formulaire
+const submit = () => {
+  // Ecoute de l'événement "click" sur le bouton
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (regexEmail.test(email.value) === false) {
+      alert("Check your email please!");
+      return;
+    } else {
+      const contact = {
+        email: email.value,
+      };
+      console.log(contact);
+      alert("Inscription confirmée!");
+      // reset formulaire
+      form.reset();
+      // Rechargement de la page
+      window.location.reload();
+    }
+  });
+};
+// Appel de la fonction submit()
+submit();
